086183643b0df4f599f6c072949edac547fb4bce
Kirk Roerig <mr.possoms@gmail.com>
2018-11-17 12:47:34 -0700

    Split compiling and symlinking the project up into two different steps

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ exo_root=$1
 rc_file=$2
 
 if [ $1 = "ci-test" ]; then
-
+	is_ci_test=true
 	exo_root=/usr/share/exo
 	rc_file=$HOME/.bashrc
 fi
@@ -17,14 +17,17 @@ dot_file_path=$(cat /tmp/$USER.dotfile)
 exo_root_path=$(cat /tmp/$USER.exo_root)
 
 if [ -f $dot_file_path ]; then
-	echo "Using dotfile: " $dot_file_path
-	
-	echo "[Dotfile contents] ---------------"
-	cat $dot_file_path
-	
-	echo "[Env vars] -----------------------"
-	printenv
+	if [ $is_ci_test = true ]; then
+		echo "Using dotfile: " $dot_file_path
 
-	EXO_ROOT=$(cat $exo_root_path) PATH="$PATH:$EXO_ROOT/bin" make install
+		echo "[Dotfile contents] ---------------"
+		cat $dot_file_path
+		
+		echo "[Env vars] -----------------------"
+		printenv
+	else
+		source $dot_file_path
+		make install
+	fi
 fi
 
