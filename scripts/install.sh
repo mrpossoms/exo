0c860bdb9507a09f46b1fd7037da7a40d20e8829
Kirk Roerig <mr.possoms@gmail.com>
2018-12-05 08:05:07 -0700

    Reverted inadvertent install script changes

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 exo_root=$1
-rc=_file=$2
-is_ci_test=false
+rc_file=$2
 
-if [ ! -z $1 ]; then
-	if [ $1 = "ci_test" ]; then 
-		is_ci_test=true
-		exo_root=/usr/share/exo
-		rc_file=$HOME/.bashrc
-	fi
+if [ $1 = "ci-test" ]; then
+	is_ci_test=true
+	exo_root=/usr/share/exo
+	rc_file=$HOME/.bashrc
 fi
 
 cd bin
@@ -20,7 +17,7 @@ dot_file_path=$(cat /tmp/$USER.dotfile)
 exo_root_path=$(cat /tmp/$USER.exo_root)
 
 if [ -f $dot_file_path ]; then
-	if [ $is_ci_test ]; then
+	if [ $is_ci_test = true ]; then
 		echo "Using dotfile: " $dot_file_path
 
 		echo "[Dotfile contents] ---------------"
