51b868ba4b439a8b06579eb1afdfdb47e14d3f9c
Kirk Roerig <mr.possoms@gmail.com>
2019-01-21 08:27:04 -0700

    Added asserts to test

--- tests/splines.cpp
+++ tests/splines.cpp
@@ -21,6 +21,11 @@ using namespace exo::math;
     auto spline = CatmullRom<float, 2>(ctrl_pts);
     spline.uniform();
 
+    Vec<float, 2> middle = { 0.5, 0 };
+    assert((spline.point(0) - ctrl_pts[1]).len() < 0.001 );
+    assert((spline.point(0.5) - middle).len() < 0.001 );
+    assert((spline.point(1) - ctrl_pts[2]).len() < 0.001 );
+
     info("C(0): %s\n",   spline.point(0).to_string().c_str());
     info("C(0.5): %s\n", spline.point(0.5).to_string().c_str());
     info("C(1): %s\n",   spline.point(1).to_string().c_str());
