0e9cc4ca370cbdd5d7584a9927f7ee6bbcfe4c2a
Kirk Roerig <mr.possoms@gmail.com>
2019-02-07 07:15:10 -0700

    Tweaks to the config test

--- tests/unix_config.cpp
+++ tests/unix_config.cpp
@@ -8,18 +8,31 @@
 
 #include "test.h"
 {
-    std::string base_path = "/tmp/test/";
-    exo::nix::data::Config cfg(base_path);
 
-    cfg["foo"].preset("1337");
+    { // create, set, open and read foo
+        std::string base_path = "/tmp/test/";
+        exo::nix::data::Config cfg(base_path);
 
-    {
-	int fd = open("/tmp/test/foo", O_RDONLY);
-	assert(fd >= 0);
-	
-	char buf[128] = {};
-	read(fd, buf, sizeof(buf));
-	assert(atoi(buf) == 1337);
+        cfg["foo"].preset("1337");
+
+        int fd = open("/tmp/test/foo", O_RDONLY);
+        assert(fd >= 0);
+
+        char buf[128] = {};
+        read(fd, buf, sizeof(buf));
+        assert(atoi(buf) == 1337);
+    }
+
+    { // try to create a config in a place off limits
+        std::string base_path = "/etc/test/";
+        exo::nix::data::Config cfg(base_path);
+
+        cfg["foo"].preset("1337");
+
+        struct passwd* pw = getpwuid(getuid());
+        const char* homedir = pw->pw_dir;
+        assert(exo::nix::fs::exists(std::string(homedir) + "/etc/test/foo") == exo::Result::OK);
+
     }
 
     return 0;
