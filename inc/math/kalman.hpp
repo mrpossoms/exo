8fe41f890d147a0bb1332e0d6742b8fdebbd948d
Kirk Roerig <mr.possoms@gmail.com>
2019-04-01 11:50:26 -0600

    WIP

--- inc/math/kalman.hpp
+++ inc/math/kalman.hpp
@@ -2,16 +2,46 @@
 
 #include "linalg.hpp"
 
+using namespace exo::math;
+
 namespace exo
 {
 namespace math
 {
 
-template <>
+template <class S, ssize_t F_SIZE, ssize_t U_SIZE, ssize_t Z_SIZE>
 struct KalmanFilter
 {
+	KalmanFilter(
+		Mat<S, F_SIZE, F_SIZE> f,
+		Mat<S, F_SIZE, U_SIZE> b,
+		Mat<S, F_SIZE, Z_SIZE> h) : F(f), B(b), H(h)
+	{
 
+	}
 
+	virtual Mat<S, 
+
+	void update()
+	{
+
+	}
+
+private:
+	// current estimate
+	Mat<S, F_SIZE> x_hat;
+	// prediction matrix
+	Mat<S, F_SIZE, F_SIZE> F;
+	// prediction co-variance matrix
+	Mat<S, F_SIZE, F_SIZE> P = Mat<S, F_SIZE, F_SIZE>::I();
+	// control matrix
+	Mat<S, F_SIZE, U_SIZE> B;
+	// unexpected noise co-variance
+	Mat<S, F_SIZE, F_SIZE> Q = Mat<S, F_SIZE, F_SIZE>::I();
+	// sensor mapping matrix
+	Mat<S, Z_SIZE, F_SIZE> H;
+	// sensor noise matrix
+	Mat<S, 
 };
 
 } // math
